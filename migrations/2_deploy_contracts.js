80da46be3741553e75ba93ee7dce6d66af5d18c8
iagpomponet <iagosilbernagel@hotmail.com>
2021-04-05 08:36:49 -0300

    wip

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,26 @@
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+// deployer - network - account (accounts are the account that u see in ganashe that are on the blockchain)
+module.exports = async function(deployer, network, accounts) {
+  // first deploy mock DAI token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  // first deploy mock DappToken
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  // deploy tokenfarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  // Transfer all tokens to TokenFarm (1 million tokes)
+  await dappToken.transfer(tokenFarm.address, "1000000000000000000000000");
+
+  // transfer 100 Mock DAI tokens to investor
+  await daiToken.transfer(accounts[1], "100000000000000000000")
 };
+
+
---
+++ src/contracts/tokenFarm.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
+contract TokenFarm {
+  // type - (public or private) - name
+  string public name = "Dapp Token Farm";
+  DappToken public dappToken;
+  DaiToken public daiToken;
+
+  constructor(DappToken _dappToken, DaiToken _daiToken) public {
+    dappToken = _dappToken;
+    daiToken = _daiToken;
+  }
+}
